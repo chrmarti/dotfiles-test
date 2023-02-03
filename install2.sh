a112cc6bf21cb040ec9a980e6a9378b6878c4090
Christof Marti <chrmarti@microsoft.com>
2023-02-03 11:40:28 +0100

    Update

---
+++ install2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "$(date): Running dotfiles install script 2." | tee -a dotfiles_install.log
--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in `seq 3`; do
-  echo dotfiles install stdout $i
-  echo dotfiles install stderr $i 1>&2
-  sleep 1
-done
-
-# base
